0a40dd8107558b8b409e74b23e1ff2148109aa87
bianavic <bianavic@gmail.com>
2019-06-28 09:37:29 -0300

    cria pokemons associados a treinadores

--- PokemonsSchema.js
+++ PokemonsSchema.js
@@ -1,26 +1,12 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const PokemonsSchema = new Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    auto: true
-  },
-  nome: {
-    type: String,
-    required: true
-  },
-  foto: {
-    type: String,
-    required: true
-  },
-  nivel: {
-    type: Number
-  }
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  nome: { type: String, required: true },
+  foto: { type: String, required: true },
+  nivel: { type: Number },
 })
 
-const pokemonsModel = mongoose.model('pokemons', PokemonsSchema)
+const pokemonsModel = mongoose.model("pokemons", PokemonsSchema);
 
-module.exports = {
-  pokemonsModel,
-  PokemonsSchema
-}
+module.exports = { pokemonsModel, PokemonsSchema };
