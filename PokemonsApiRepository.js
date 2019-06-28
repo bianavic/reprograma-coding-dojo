c1c761aff9a79553d834e2aa1d62aaa9700bd913
bianavic <bianavic@gmail.com>
2019-06-28 09:42:20 -0300

    adiciona env do mongo no repositorio

--- PokemonsApiRepository.js
+++ PokemonsApiRepository.js
@@ -1,18 +1,18 @@
 require('dotenv-safe').load()
-const mongoose = require("mongoose");
-const MONGO_URL = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+const MONGO_URL = process.env.MONGODB_URI
 
 function connect () {
   mongoose.connect(MONGO_URL,
     { useNewUrlParser: true },
     function (error) {
-      if(error) {
-        console.error("Algo de errado não está certo: ", error)
+      if (error) {
+        console.error('Algo de errado não está certo: ', error)
       } else {
-        console.log("Conectado no mongoDB.")
+        console.log('Conectado no mongoDB.')
       }
     }
-  );
+  )
 }
 
 module.exports = { connect }
